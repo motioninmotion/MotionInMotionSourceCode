17972780fbf930e081ba1e63d7ec837f4e248e96
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2013-11-20 03:04:29 +1100

    Implemented the table view delegate and data source

--- Episode-2/Blapp/app/controllers/PostsController.rb
+++ Episode-2/Blapp/app/controllers/PostsController.rb
@@ -12,10 +12,34 @@ class PostsController < UITableViewController
   end
 
   def viewDidLoad
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
   end
 
   def viewDidAppear(animated)
     super
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    @postsModel.records.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    @reuseIdentifier ||= "PostsCell"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:@reuseIdentifier)
+    end
+
+    cell.textLabel.text = @postsModel.records[indexPath.row][:author]
+    cell.detailTextLabel.text = @postsModel.records[indexPath.row][:content]
+
+    cell
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    postDetailController = PostDetailController.alloc.initWithPostDetails(@postsModel.records[indexPath.row])
+    self.navigationController.pushViewController(postDetailController, animated:true)
   end
 
 end
