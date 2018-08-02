cff714076ee9baf7875583c65b37d7da0a535a26
Derek Melchin <derek@wndx.com>
2018-08-02 13:41:16 -0600

    Update to match episode-5

--- Episode-10/after/Blapp/app/delegates/PostsControllerAlertViewDelegate.rb
+++ Episode-10/after/Blapp/app/delegates/PostsControllerAlertViewDelegate.rb
@@ -1,14 +1,14 @@
 module PostsControllerAlertViewDelegate
-  def create_new_post(title)
-    Post.create({title: title, content: '', created_at: Time.now, updated_at: Time.now, pseudonym: Pseudonym.first})
-    cdq.save
-    self.tableView.insertRowsAtIndexPaths([NSIndexPath.indexPathForRow(Post.count, inSection:0)], withRowAnimation:UITableViewRowAnimationTop)
-  end
-
-  def update_post(post, with_title: new_title)
-    Post.all[post].title = new_title
-    Post.all[post].updated_at = Time.now
-    cdq.save
-    self.tableView.reloadRowsAtIndexPaths([NSIndexPath.indexPathForRow(post + 1, inSection:0)], withRowAnimation: UITableViewRowAnimationFade)
+  def configure_post(title, post=nil)
+    if post
+      Post.all[post].title = title
+      Post.all[post].updated_at = Time.now
+      cdq.save
+      self.tableView.reloadRowsAtIndexPaths([NSIndexPath.indexPathForRow(post + 1, inSection:0)], withRowAnimation: UITableViewRowAnimationFade)
+    else
+      Post.create({title: title, content: '', created_at: Time.now, updated_at: Time.now, pseudonym: Pseudonym.first})
+      cdq.save
+      self.tableView.insertRowsAtIndexPaths([NSIndexPath.indexPathForRow(Post.count, inSection:0)], withRowAnimation:UITableViewRowAnimationTop)
+    end
   end
 end
