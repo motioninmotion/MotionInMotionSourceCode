6f794884c4fd84d8eebb09b3041a639bc4d7bd0f
Derek Melchin <derek@wndx.com>
2018-08-02 13:18:37 -0600

    Remove duplicate code

--- Episode-5/after/Blapp/app/delegates/PostsControllerTableViewDelegate.rb
+++ Episode-5/after/Blapp/app/delegates/PostsControllerTableViewDelegate.rb
@@ -4,27 +4,25 @@ module PostsControllerTableViewDelegate
     tableView.deselectRowAtIndexPath(indexPath, animated:true)
 
     if indexPath.row == 0
-      alert = UIAlertController.alertControllerWithTitle('Post Title',
-                                                         message: 'Enter a title for your Blapp post',
-                                                         preferredStyle: UIAlertControllerStyleAlert)
-      cancel_action = UIAlertAction.actionWithTitle('Cancel', style: UIAlertActionStyleCancel, handler: nil)
-      continue_action = UIAlertAction.actionWithTitle('Continue', style: UIAlertActionStyleDefault, handler: lambda { |_| create_new_post(alert.textFields[0].text)})
-      alert.addTextFieldWithConfigurationHandler(lambda {|_|})
-      alert.addAction(cancel_action)
-      alert.addAction(continue_action)
-      presentViewController(alert, animated: true, completion: nil)
+      gather_post_input
     else
       self.navigationController.pushViewController(PostDetailController.alloc.initWithPost(Post.all[indexPath.row - 1]), animated:true)
     end
   end
 
   def tableView(tableView, accessoryButtonTappedForRowWithIndexPath:indexPath)
-    alert = UIAlertController.alertControllerWithTitle('Edit Post Title',
+    gather_post_input(indexPath.row - 1)
+  end
+
+  def gather_post_input(row = nil)
+    alert = UIAlertController.alertControllerWithTitle(row ? 'Edit Post Title' : 'Post Title',
                                                        message: 'Enter a title for your Blapp post',
                                                        preferredStyle: UIAlertControllerStyleAlert)
     cancel_action = UIAlertAction.actionWithTitle('Cancel', style: UIAlertActionStyleCancel, handler: nil)
-    continue_action = UIAlertAction.actionWithTitle('Continue', style: UIAlertActionStyleDefault, handler: lambda { |_| update_post(indexPath.row - 1, with_title: alert.textFields[0].text)})
-    alert.addTextFieldWithConfigurationHandler(lambda {|text_field| text_field.text = Post.all[indexPath.row - 1].title})
+    continue_handler = lambda { |_| configure_post(alert.textFields[0].text, row)}
+    configuration_handler = row ? lambda {|text_field| text_field.text = Post.all[row].title} : lambda {|_|}
+    continue_action = UIAlertAction.actionWithTitle('Continue', style: UIAlertActionStyleDefault, handler: continue_handler)
+    alert.addTextFieldWithConfigurationHandler(configuration_handler)
     alert.addAction(cancel_action)
     alert.addAction(continue_action)
     presentViewController(alert, animated: true, completion: nil)
